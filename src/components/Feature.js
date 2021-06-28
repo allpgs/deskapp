771f0685d3e0b81e92e401e809e5a9307d873ac1
All PGS <kms0219kms@naver.com>
2021-06-29 06:14:34 +0900

    ㄷㄷ

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -10,7 +10,7 @@ class Feature extends React.Component {
                 <TableCell><img src={"https://raw.githubusercontent.com/" + this.props.github + "/master/logo.png"} alt="logo" width="64px" height="64px" /></TableCell>
                 <TableCell>{this.props.name}</TableCell>
                 <TableCell><a href={this.props.sites}>{this.props.alt}</a></TableCell>
-                <TableCell><a href={"https://github.com/" + this.props.github + "/releases/tag/"}>Download</a></TableCell>
+                <TableCell><a href={"https://github.com/" + this.props.github + "/releases"}>Download</a></TableCell>
                 <TableCell><a href={"https://github.com/" + this.props.github}>{this.props.github}</a></TableCell>
             </TableRow>
         )
